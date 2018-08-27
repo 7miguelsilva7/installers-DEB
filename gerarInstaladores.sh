a306a275bc91eaa28751c1dd11f73bb622e42afb
7miguelsilva7 <7miguelsilva7@gmail.com>
2018-08-27 15:45:39 -0300

    update

--- gerarInstaladores.sh
+++ gerarInstaladores.sh
@@ -1,71 +1,124 @@
 #!/bin/bash
 date=`date +%Y-%m`
-(
-echo 'Executando tarefa, aguarde... '
 
-mkdir 0-instaladores
-rm 0-instaladores/*.deb
+echo "Escolha uma Opção: 
+1 - Gerar SimRede 
+2 - Gerar Firefox
+3 - Alternate Panel
+4 - Atv-Jclic
+5 - Atv-Jclic - Lousa Interativa
+6 - Busque Aqui
+7 - Config Rede
+8 - CustomISO
+9 - EditorISO
+10 - EdujogatiFX
+11 - EdujogatiFX-Diebold
+12 - EdujogatiFX-Lousa-Interativa
+13 - Epoptes-Reconect
+14 - GeloDesktop
+15 - Liga-Projetor-Diebold
+16 - TODOS
+"
 
-#gerar alternatePanel
-dpkg-deb -b -Z gzip alternatePanel 0-instaladores/alternatePanel-$date.deb
+read num
 
-#gerar atvJcic
-dpkg-deb -b -Z gzip atvJClic 0-instaladores/atv-jclic-$date.deb
-
-
-#gera AtvJclicLousaInterativa
-dpkg-deb -b -Z gzip atvJclicLousaInterativa 0-instaladores/atvJclicLousaInterativa-$date.deb
-
-
-#gera busqueAqui
-dpkg-deb -b -Z gzip busqueAqui 0-instaladores/busqueAqui-$date.deb
-
-
-#gera Conf_rede
-dpkg-deb -b -Z gzip conf_rede 0-instaladores/configRede-$date.deb
-
-
-#gera CustomISO
-dpkg-deb -b -Z gzip customISO 0-instaladores/customISO-$date.deb
-
-
-#installerEditorISO
-dpkg-deb -b -Z gzip editorISO-menuImagemClonezila 0-instaladores/editorISO-menuImagemClonezila-$date.deb
-
-
-#gera edujogatiFx
-dpkg-deb -b -Z gzip edujogatiFX 0-instaladores/edujogatiFX-$date.deb
-
-#gera edujogatiFXDiebold
-dpkg-deb -b -Z gzip edujogatiFXDiebold 0-instaladores/edujogatiFXDiebold-$date.deb
-
-
-#gera EdujogatiFXLousaInterativa
-dpkg-deb -b -Z gzip epoptesReconect 0-instaladores/epoptesReconect-$date.deb
-
-
-#epoptes-Reconect
-dpkg-deb -b -Z gzip edujogatiFXLousaInterativa 0-instaladores/edujogatiFXLousaInterativa-$date.deb
-
-
-#gera geloDesktop
-dpkg-deb -b -Z gzip geloDesktop 0-instaladores/geloDesktop-$date.deb
-
-
-#gera LigaLampProjDiebold
-dpkg-deb -b -Z gzip ligaLampProjDiebold 0-instaladores/ligaLampProjDiebold-$date.deb
-
-
-#gera simRede
+if [ $num == 1 ];
+then
 dpkg-deb -b -Z gzip simRede 0-instaladores/simRede-$date.deb
+fi
 
-#gera firefox 45.0.2
+if [ $num == 2 ];
+then
 dpkg-deb -b -Z gzip firefox-45.0.2 0-instaladores/firefox-45.0.2-$date.deb
+fi
 
-#cd ..
-#xterm -e bash ssh-installers.sh
+if [ $num == 3 ];
+then
+dpkg-deb -b -Z gzip alternatePanel 0-instaladores/alternatePanel-$date.deb
+fi
 
-sleep 1
-) | zenity --progress --width 300 --height 100 --title "Informação do Sistema" --text "Executando tarefa, aguarde... !!!" --pulsate --auto-close
+if [ $num == 4 ];
+then
+dpkg-deb -b -Z gzip atvJClic 0-instaladores/atv-jclic-$date.deb
+fi
 
-zenity --info --width 300 --height 100 --title "Informação do Sistema" --text "Tarefas Finalizadas"
+if [ $num == 5 ];
+then
+dpkg-deb -b -Z gzip atvJclicLousaInterativa 0-instaladores/atvJclicLousaInterativa-$date.deb
+fi
+
+if [ $num == 6 ];
+then
+dpkg-deb -b -Z gzip busqueAqui 0-instaladores/busqueAqui-$date.deb
+fi
+
+if [ $num == 7 ];
+then
+dpkg-deb -b -Z gzip conf_rede 0-instaladores/configRede-$date.deb
+fi
+
+if [ $num == 8 ];
+then
+dpkg-deb -b -Z gzip customISO 0-instaladores/customISO-$date.deb
+fi
+
+if [ $num == 9 ];
+then
+dpkg-deb -b -Z gzip editorISO-menuImagemClonezila 0-instaladores/editorISO-menuImagemClonezila-$date.deb
+fi
+
+if [ $num == 10 ];
+then
+dpkg-deb -b -Z gzip edujogatiFX 0-instaladores/edujogatiFX-$date.deb
+fi
+
+if [ $num == 11 ];
+then
+dpkg-deb -b -Z gzip edujogatiFXDiebold 0-instaladores/edujogatiFXDiebold-$date.deb
+fi
+
+if [ $num == 12 ];
+then
+dpkg-deb -b -Z gzip edujogatiFXLousaInterativa 0-instaladores/edujogatiFXLousaInterativa-$date.deb
+fi
+
+if [ $num == 13 ];
+then
+dpkg-deb -b -Z gzip epoptesReconect 0-instaladores/epoptesReconect-$date.deb
+fi
+
+if [ $num == 14 ];
+then
+dpkg-deb -b -Z gzip geloDesktop 0-instaladores/geloDesktop-$date.deb
+fi
+
+if [ $num == 15 ];
+then
+dpkg-deb -b -Z gzip ligaLampProjDiebold 0-instaladores/ligaLampProjDiebold-$date.deb
+fi
+
+if [ $num == 16 ];
+then
+dpkg-deb -b -Z gzip simRede 0-instaladores/simRede-$date.deb
+dpkg-deb -b -Z gzip refox-45.0.2 0-instaladores/refox-45.0.2-$date.deb
+dpkg-deb -b -Z gzip alternatePanel 0-instaladores/alternatePanel-$date.deb
+dpkg-deb -b -Z gzip atvJClic 0-instaladores/atv-jclic-$date.deb
+dpkg-deb -b -Z gzip atvJclicLousaInterativa 0-instaladores/atvJclicLousaInterativa-$date.deb
+dpkg-deb -b -Z gzip busqueAqui 0-instaladores/busqueAqui-$date.deb
+dpkg-deb -b -Z gzip conf_rede 0-instaladores/congRede-$date.deb
+dpkg-deb -b -Z gzip customISO 0-instaladores/customISO-$date.deb
+dpkg-deb -b -Z gzip editorISO-menuImagemClonezila 0-instaladores/editorISO-menuImagemClonezila-$date.deb
+dpkg-deb -b -Z gzip edujogatiFX 0-instaladores/edujogatiFX-$date.deb
+dpkg-deb -b -Z gzip edujogatiFXDiebold 0-instaladores/edujogatiFXDiebold-$date.deb
+dpkg-deb -b -Z gzip edujogatiFXLousaInterativa 0-instaladores/edujogatiFXLousaInterativa-$date.deb
+dpkg-deb -b -Z gzip epoptesReconect 0-instaladores/epoptesReconect-$date.deb
+dpkg-deb -b -Z gzip geloDesktop 0-instaladores/geloDesktop-$date.deb
+dpkg-deb -b -Z gzip ligaLampProjDiebold 0-instaladores/ligaLampProjDiebold-$date.deb
+fi
+
+
+
+
+
+
+
